2a6891794cd6f7b2c2bc1cd67d8b303cfc5b387c
muhammad-rk-isa <muhammad-rk-isa@outook.com>
2023-09-23 21:28:27 +0600

    File structure changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
-      Hello from Next.js 13.5.2 '
+      Hello from Next.js 13.5.2
+      <Button className="ml-8">
+        Click Me
+      </Button>
     </main>
   )
 }
